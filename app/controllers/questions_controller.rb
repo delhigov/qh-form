43a123cd45c08d9364a1efc2033ff542a3db9dc9
catherinexu1 <catherinexu1996@gmail.com>
2015-10-31 05:53:51 -0700

    separate personal info and question pages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,23 +8,28 @@ class QuestionsController < ApplicationController
 
 		form = params[:question]
 
-		@question.title = form[:title]
-		@question.ministry = form[:ministry]
-		@question.text = form[:text]
-		if !@question.valid?
-			flash[:notice] = []
-			@question.errors[:question].each do |e|
-				flash[:notice].push(e)
-			end
-			redirect_to :controller => 'questions', :action => 'new'
-		else
-			@question.save()
-			redirect_to root_path
-		end
+		# @question.title = form[:title]
+		# @question.ministry = form[:ministry]
+		# @question.text = form[:text]
+		# if !@question.valid?
+		# 	flash[:notice] = []
+		# 	@question.errors[:question].each do |e|
+		# 		flash[:notice].push(e)
+		# 	end
+		# 	redirect_to :controller => 'questions', :action => 'question'
+		# else
+		# 	@question.save()
+		# 	redirect_to root_path
+		# end
   end
 
   def new
   	@question = Question.new
+ #  	form = params[:question]
+	# @question.name = form[:name]
+	# @question.email = form[:email]
+	# @question.district = form[:district]
+	# @question.state = form[:state]
   end
 
   
