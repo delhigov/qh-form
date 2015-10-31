67193a08a5045dc49854cc3dae58b0744a9988b2
catherinexu1 <catherinexu1996@gmail.com>
2015-10-31 02:41:51 -0700

    pulling from master

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -20,27 +20,6 @@ class QuestionsController < ApplicationController
 		end
   end
 
-  def user
-		question = Question.new
-
-		if @question.blank?
-			flash[:notice] = "Error! Please enter a valid question"
-		end
-
-		question.ministry = params[:ministry]
-		question.text = params[:text]
-		if !question.valid?
-			flash[:notice] = []
-			user.errors[:user].each do |e|
-				flash[:notice].push(e)
-			end
-			redirect_to :controller => 'questions', :action => 'new'
-		else
-			question.save()
-			redirect_to root_path
-		end
-  end
-
   def new
   	@question = Question.new
   end
