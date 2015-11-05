629f4b31037f090794f074508eafa9ffe88f58db
catherinexu1 <catherinexu1996@gmail.com>
2015-11-05 01:16:04 -0800

    fix back button

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,12 @@
 class QuestionsController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+  
+  def show
+    @question = Question.find(params[:id])
+  end
+
   def new
     session[:question_params] ||= {}
     @question = Question.new(session[:question_params])
