5d392e52d658e32bf5d89fa61336532a37fd546c
catherinexu1 <catherinexu1996@gmail.com>
2015-11-05 03:43:11 -0800

    add pdf option

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module Qhform
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.use 'PDFKit::Middleware'
+    config.middleware.use 'PDFKit::Middleware', :print_media_type => true
   end
 end
